29cadf8a2f03b51ee10b978871a672629d8a2fbe
Jonathan Bradshaw <lavin@posteo.de>
2017-03-25 22:48:08 -0700

    Complete stringifyJSON test

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 
+// Base cases are numbers, strings, booleans, and null
+// Recursion occurs in arrays and objects
+// For strings, replace quotation marks with escaped marks
+// For array, create temp array, stringify each element and pass to temp array. Return temp array.  
+// For object, create temp array, stringigy key and value pairs, push to temp array and return. 
+// 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'number') {
+  	return obj.toString();
+  } else 
+  if (typeof obj === 'boolean') {
+  	return obj.toString();
+  } else 
+  if (obj === null) {
+  	return 'null';
+  } else
+  if (typeof obj === 'string') {
+  	return "\"" + obj + "\"";
+  } else 
+  if (Array.isArray(obj)){
+  	var tempObj = obj.map( element => stringifyJSON(element));
+	return "[" + tempObj + "]";
+  } else
+  if (typeof obj === 'object'){
+  	var result = [];
+
+  	for(var key in obj){
+  		if(key === 'functions' || key === 'undefined'){
+  			return "{}";
+  		} else
+  		if(obj.hasOwnProperty(key)){
+  			result.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key])); 
+  		}
+  	}
+
+  	return "{" + result.join(",") + "}";
+  }
 };
